a592c4632d625347c5730e8b901fc565ed580b08
Coy <coyreid7@gmail.com>
2024-06-28 05:11:50 -0500

    fixed homepage error

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './HomePage'; // Adjust the import path according to your file structure
+import HomePage from './components/HomePage';
 import AppBar from '@mui/material/AppBar';
 
 const App = () => {
