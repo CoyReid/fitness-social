2b380304dfd62493550b10e19c0188c9bc2613b6
Coy <coyreid7@gmail.com>
2024-07-02 03:39:20 -0500

    working on client side authentication

--- client/src/App.js
+++ client/src/App.js
@@ -18,3 +18,28 @@ const App = () => {
 };
 
 export default App;
+
+
+// import axios from 'axios';
+
+// const API_URL = '/api/auth/';
+
+// const login = (username, password) => {
+//     return axios
+//         .post(API_URL + 'signin', { username, password })
+//         .then((response) => {
+//             if (response.data.accessToken) {
+//                 localStorage.setItem('user', JSON.stringify(response.data));
+//             }
+//             return response.data;
+//         });
+// };
+
+// const logout = () => {
+//     localStorage.removeItem('user');
+// };
+
+// export default {
+//     login,
+//     logout,
+// };
