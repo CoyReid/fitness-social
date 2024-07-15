dc78baa2d00f0bce5eb8cb66841ed5e55afd1054
Coy <coyreid7@gmail.com>
2024-07-15 10:05:57 -0500

    further planning

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,3 +1,4 @@
 //will be default page for user to see their profile
 //Add starter code and an edit button to change details
 //Add a delete button to delete account
+//Profile page can comewayyyy later though once back end and other pages are working properly and as intended
